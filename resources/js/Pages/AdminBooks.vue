b4b7850dd5a260c20752961bed7b0c5c1b5ae456
oTales <talesirineu@gmail.com>
2023-12-07 20:05:28 -0300

    comitando para fazer o teste do filament

---
+++ resources/js/Pages/AdminBooks.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps(['books']);
+</script>
+
+<template>
+    <ul class="flex">
+        <div>
+            <li>
+                <img :src="book.image" :alt="book.title" />
+            </li>
+            <li>
+                <h2>{{ book.title }}</h2>
+                <h3>{{ book.author }}</h3>
+                <p>{{ book.description }}</p>
+            </li>
+        </div>
+        <div>
+            <li>Update</li>
+            <li>Delete</li>
+        </div>
+    </ul>
+
+</template>
